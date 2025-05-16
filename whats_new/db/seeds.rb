203446274f75d0b5824661d5cd722441ebf4de82
DanielaMartinezD <damartinez6@miuandes.cl>
2025-05-16 19:40:13 -0400

    lab08 db

--- whats_new/db/seeds.rb
+++ whats_new/db/seeds.rb
@@ -8,176 +8,59 @@
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
 
-User.create!(
-  email: "ohanalilo@disney.com",
-  first_name: "Lilo",
-  last_name: "Pelekai"
-)
+lilo = User.create!(email: "ohanalilo@disney.com", first_name: "Lilo", last_name: "Pelekai")
+stitch = User.create!(email: "stitch@disney.com", first_name: "Stitch", last_name: "Pelekai")
+hello = User.create!(email: "hellokitty@sanrio.com", first_name: "Hello", last_name: "Kitty")
+cinnamon = User.create!(email: "cinnamon@sanrio.com", first_name: "Cinnamon", last_name: "Roll")
+hiccup = User.create!(email: "hiccup@dreamworks.com", first_name: "Hiccup", last_name: "Haddock")
+astrid = User.create!(email: "astrid.h@dreamworks.com", first_name: "Astrid", last_name: "Hofferson")
+violet = User.create!(email: "violetparr@pixar.com", first_name: "Violet", last_name: "Parr")
+jack = User.create!(email: "jack2@pixar.com", first_name: "Jack-Jack", last_name: "Parr")
+mickey = User.create!(email: "mickey50@disney.com", first_name: "Mickey", last_name: "Mouse")
+minnie = User.create!(email: "mrsminnie@jyp.com", first_name: "Minnie", last_name: "Mouse")
 
-User.create!(
-  email: "stitch@disney.com",
-  first_name: "Stitch",
-  last_name: "Pelekai"
-)
-
-User.create!(
-  email: "hellokitty@sanrio.com",
-  first_name: "Hello",
-  last_name: "Kitty"
-)
-
-User.create!(
-  email: "cinnamon@sanrio.com",
-  first_name: "Cinnamon",
-  last_name: "Roll"
-)
-
-User.create!(
-  email: "hiccup@dreamworks.com",
-  first_name: "Hiccup",
-  last_name: "Haddock"
-)
-
-User.create!(
-  email: "astrid.h@dreamworks.com",
-  first_name: "Astrid",
-  last_name: "Hofferson"
-)
-
-User.create!(
-  email: "violetparr@pixar.com",
-  first_name: "Violet",
-  last_name: "Parr"
-)
-
-User.create!(
-  email: "jack2@pixar.com",
-  first_name: "Jack-Jack",
-  last_name: "Parr"
-)
-
-User.create!(
-  email: "mickey50@disney.com",
-  first_name: "Mickey",
-  last_name: "Mouse"
-)
-
-User.create!(
-  email: "mrsminnie@jyp.com",
-  first_name: "Minnie",
-  last_name: "Mouse"
-)
 
 #--------------------------------------------------------------
 
-Chat.create!(
-  sender_id: User.first.id,
-  receiver_id: User.second.id
-)
-
-Chat.create!(
-  sender_id: User.second.id,
-  receiver_id: User.third.id
-)
-
-Chat.create!(
-  sender_id: User.third.id,
-  receiver_id: User.fourth.id
-)
-
-Chat.create!(
-  sender_id: User.fifth.id,
-  receiver_id: User.fourth.id
-)
-
-Chat.create!(
-  sender_id: User.first.id,
-  receiver_id: User.fifth.id
-)
-
-Chat.create!(
-  sender_id: User.second.id,
-  receiver_id: User.first.id
-)
-
-Chat.create!(
-  sender_id: User.second.id,
-  receiver_id: User.fourth.id
-)
-
-Chat.create!(
-  sender_id: User.third.id,
-  receiver_id: User.first.id
-)
-
-Chat.create!(
-  sender_id: User.first.id,
-  receiver_id: User.third.id
-)
-
-Chat.create!(
-  sender_id: User.fourth.id,
-  receiver_id: User.first.id
-)
+chat1 = Chat.create!(sender_id: lilo.id, receiver_id: stitch.id)
+chat2 = Chat.create!(sender_id: stitch.id, receiver_id: hello.id)
+chat3 = Chat.create!(sender_id: hello.id, receiver_id: cinnamon.id)
+chat4 = Chat.create!(sender_id: hiccup.id, receiver_id: astrid.id)
+chat5 = Chat.create!(sender_id: violet.id, receiver_id: jack.id)
+chat6 = Chat.create!(sender_id: mickey.id, receiver_id: minnie.id)
+chat7 = Chat.create!(sender_id: minnie.id, receiver_id: violet.id)
+chat8 = Chat.create!(sender_id: jack.id, receiver_id: mickey.id)
+chat9 = Chat.create!(sender_id: cinnamon.id, receiver_id: lilo.id)
+chat10 = Chat.create!(sender_id: astrid.id, receiver_id: stitch.id)
 
 #-----------------------------------------------------------
 
-Message.create!(
-  chat_id: Chat.first.id,
-  user_id: User.find_by(first_name: "Lilo").id,
-  body: "Hey Stitch, did you eat my sandwich again?!"
-)
+Message.create!(chat_id: chat1.id, user_id: lilo.id, body: "Hey Stitch, did you eat my sandwich again?!")
+Message.create!(chat_id: chat1.id, user_id: stitch.id, body: "delicious")
 
-Message.create!(
-  chat_id: Chat.first.id,
-  user_id: User.find_by(first_name: "Stitch").id,
-  body: "delicious"
-)
+Message.create!(chat_id: chat2.id, user_id: stitch.id, body: "Hey Hello Kitty!")
+Message.create!(chat_id: chat2.id, user_id: hello.id, body: "Hi Stitch! Want to draw together?")
 
-Message.create!(
-  chat_id: Chat.second.id,
-  user_id: User.find_by(first_name: "Hello").id,
-  body: "Cinnamon! Want to go get some muffins later?"
-)
+Message.create!(chat_id: chat3.id, user_id: hello.id, body: "Cinnamon! Want to go get some muffins later?")
+Message.create!(chat_id: chat3.id, user_id: cinnamon.id, body: "Yesss! Sanrio Café at 4")
 
-Message.create!(
-  chat_id: Chat.second.id,
-  user_id: User.find_by(first_name: "Cinnamon").id,
-  body: "Yesss! Sanrio Café at 4"
-)
+Message.create!(chat_id: chat4.id, user_id: hiccup.id, body: "Have you seen Toothless?")
+Message.create!(chat_id: chat4.id, user_id: astrid.id, body: "Yes! Toothless is flying over the mountains")
 
-Message.create!(
-  chat_id: Chat.third.id,
-  user_id: User.find_by(first_name: "Hiccup").id,
-  body: "Have you seen Toothless?"
-)
+Message.create!(chat_id: chat5.id, user_id: violet.id, body: "Jack-Jack, please be careful")
+Message.create!(chat_id: chat5.id, user_id: jack.id, body: "*baby giggle*")
 
-Message.create!(
-  chat_id: Chat.third.id,
-  user_id: User.find_by(first_name: "Astrid").id,
-  body: "Yes! Toothless is flying over the mountains"
-)
+Message.create!(chat_id: chat6.id, user_id: mickey.id, body: "Hello Minnie!")
+Message.create!(chat_id: chat6.id, user_id: minnie.id, body: "Hello Mickey <3")
 
-Message.create!(
-  chat_id: Chat.fourth.id,
-  user_id: User.find_by(first_name: "Violet").id,
-  body: "Jack-Jack, please be careful"
-)
+Message.create!(chat_id: chat7.id, user_id: minnie.id, body: "Hi Violet! Want to go shopping?")
+Message.create!(chat_id: chat7.id, user_id: violet.id, body: "Sure! Let's meet at the mall at 5.")
 
-Message.create!(
-  chat_id: Chat.fourth.id,
-  user_id: User.find_by(first_name: "Jack-Jack").id,
-  body: "*baby giggle*"
-)
+Message.create!(chat_id: chat8.id, user_id: jack.id, body: "*baby giggle*")
+Message.create!(chat_id: chat8.id, user_id: mickey.id, body: "Haha! You're so cute, Jack-Jack.")
 
-Message.create!(
-  chat_id: Chat.fifth.id,
-  user_id: User.find_by(first_name: "Mickey").id,
-  body: "Hello Minnie!"
-)
+Message.create!(chat_id: chat9.id, user_id: cinnamon.id, body: "Hey Lilo, want to bake some cupcakes together?")
+Message.create!(chat_id: chat9.id, user_id: lilo.id, body: "Yes please!")
 
-Message.create!(
-  chat_id: Chat.fifth.id,
-  user_id: User.find_by(first_name: "Minnie").id,
-  body: "Hello Mickey <3"
-)
+Message.create!(chat_id: chat10.id, user_id: astrid.id, body: "Stitch, you're needed for dragon training.")
+Message.create!(chat_id: chat10.id, user_id: stitch.id, body: "Dragon! I ride now!")
